3096bb60cf4eeaab0eb68783595da3b8b209735a
must-git-good <39744893+must-git-good@users.noreply.github.com>
2018-08-08 09:49:53 -0500

    pushing from local machine to git

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,127 +1,114 @@
 //
 
+var topics = ["Cats", "Dogs", "Monkeys"];
 
-var topics = ["Test","Cats","Dobermans"];
-
-
-
-
+var input = null;
+var userInput = topics[(topics.length - 1)];
 
 
+for (var j = 0; j < topics.length; j++) {
+    var newBootstrapAccordion = '<div class="card" id="cardnum-' + j + '"><div class="card-header" id=' + j + '><h5 class="mb-0"><button id=' + topics[j] + ' class="btn btn-link collapsed accordion-btn" type="button" data-toggle="collapse" data-target="#collapse' + j + '" aria-expanded="false" aria-controls="collapse' + j + '">' + topics[j] + ' </button></h5></div><div id="collapse' + j + '"class="collapse" aria-labelledby="heading"' + j + ' data-parent="#accordionHolder"><div class="card-body"><div class="gif-content" id="gif-"></div></div></div></div></div>'
+
+    $("#accordionHolder").append(newBootstrapAccordion);
+
+};
 
 
-var userSearch = "test";
-// var userSearch = $("#search-results").val();
-var searchTerm = userSearch;
+var pushAccordion = function () {
+    for (var j = 0; j < topics.length; j++) {
+        var newBootstrapAccordion = '<div class="card" id="cardnum-' + j + '"><div class="card-header" id=' + j + '><h5 class="mb-0"><button id=' + topics[j] + ' class="btn btn-link collapsed accordion-btn" type="button" data-toggle="collapse" data-target="#collapse' + j + '" aria-expanded="false" aria-controls="collapse' + j + '">' + topics[j] + ' </button></h5></div><div id="collapse' + j + '"class="collapse" aria-labelledby="heading"' + j + ' data-parent="#accordionHolder"><div class="card-body"><div class="gif-content" id="gif-"></div></div></div></div></div>'
 
-resultsAmount = 10;
+        $("#accordionHolder").append(newBootstrapAccordion);
 
-var urlQuery = "https://api.giphy.com/v1/gifs/search?q=" + searchTerm + "&api_key=kFmrkggeHb0rVjSwBsYc3fImxxJKPqWA&limit=" + resultsAmount;
-
-
-$.ajax({
-    url: urlQuery,
-    method: "GET",
-}).then(function(apiData){
-    var data = apiData.data;
-
-    console.log(data);
-    
-
-    for( var i = 0; i < data.length; i++){
-       
-    var stillImg = data[i].images.fixed_height_small_still.url;
-    var moveImg = data[i].images.fixed_height_small.url;
-        if (data[i].rating !== "r") {
-            var gifHolder = $("<div class='item'>");
-            var rating = data[i].rating;
-            var p = $("<p>").text("Rating: " + rating);
-            var image = $("<img>");
-            image.attr({
-                "class": "gif",
-                "id": i,
-                "src": data[i].images.fixed_height_small_still.url,
-                "move-src": data[i].images.fixed_height_small.url,
-                "static-src": data[i].images.fixed_height_small_still.url,
-                "data-state": "still",
-            });
-            gifHolder.append(p);
-            gifHolder.append(image);
-            $(".gif-content").prepend(gifHolder);
+    }; // end of accordion populate for-loop
+}
 
 
 
 
-            var listHold = $("#accordionHolder");
-            var mkCard = $("<div class='card'>");
-            var mkCardStep1 = $("<div class='card-header'>");
-            var mkCardStep2 = $("<h5 class='mb-0'>");
-            var mkCardBtn1 = $('<button class="btn btn-link" type="button" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="true" aria-controls="collapseTwo"');
-            var mkCardStep3 = $('<div id="collapseTwo" class="collapse show" aria-labelledby="heading"'+i+'data-parent="#accordionHolder">');
-            var mkCardStep4 = $('<div class="card-body gif-content" id="gif-">'+i);
-
-            mkCardStep4;
-            mkCardStep3;
-            mkCardBtn1.text(userSearch);
-            mkCardStep2;
-            mkCardStep1;
-
-        
-            mkCardStep2.append(mkCardBtn1);
-            mkCardStep1.append(mkCardStep2);
-            mkCard.append(mkCardStep1);
-
-
-            mkCardStep3.append(mkCardStep4);
-            mkCard.append(mkCardStep3);
-            
-            listHold.append(mkCard);
-
-            //         <div class="card-body">')
-                                    //  <div class="card">
-                                    //       <div class="card-header" id="headingOne">
-                                    //         <h5 class="mb-0">
-                                    //           <button class="btn btn-link" type="button" data-toggle="collapse" data-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
-                                    //              Words
-                                    //           </button>
-                                    //         </h5>
-                                    //       </div>
-                                      
-                                    //       <div id="collapseOne" class="collapse show" aria-labelledby="headingOne" data-parent="#accordionExample">
-                                    //         <div class="card-body">
-                                    //                     <div class="middle-form" id="middle">
-
-                                    //                             </div>
-                                    //         </div>
-                                    //       </div>
-                                    // </div>
-
-
-
-
-
-
-        }
-        $(".gif").val(this).on("click", function() {
-            // The attr jQuery method allows us to get or set the value of any attribute on our HTML element
-            var state = $(this).attr("data-state");
-            console.log(this);
-            console.log($(".gif"));
-            console.log($(".gif").val());
-            // If the clicked image's state is still, update its src attribute to what its data-animate value is.
-            // Then, set the image's data-state to animate
-            // Else set src to the data-still value
-            if (state === "still") {
-              $(this).attr("src", $(this).attr("move-src"));
-              $(this).attr("data-state", "move");
-            } else {
-              $(this).attr("src", $(this).attr("static-src"));
-              $(this).attr("data-state", "still");
-            }
-          });
+var inputCheck = function () {
+    if (topics.includes(userInput)) {
+        alert("That's already listed!");
+    } if (userInput === "") {
+        alert("Please enter a search term.");
+    } else if ((topics.includes(userInput) === false) && (userInput !== "")) {
+        topics.push(userInput);
     }
-   
+};
 
+
+$(document).on("click", ".accordion-btn", function () {
+    console.log("I clicked the ACCORDION.")
+    console.log(this);
+    console.log(this.id);
+
+    var searchTerm = this.id;
+    resultsAmount = 10;
+    var urlQuery = "https://api.giphy.com/v1/gifs/search?q=" + searchTerm + "&api_key=kFmrkggeHb0rVjSwBsYc3fImxxJKPqWA&limit=" + resultsAmount;
+
+    $.ajax({
+        url: urlQuery,
+        method: "GET",
+    }).then(function (apiData) {
+        var data = apiData.data;
+
+        for (var i = 0; i < data.length; i++) {
+            if (data[i].rating !== "r") {
+                var gifHolder = $("<div class='item'>");
+                var rating = data[i].rating;
+                var p = $("<p>").text("Rating: " + rating);
+                var image = $("<img>");
+                image.attr({
+                    "class": "gif",
+                    "id": i,
+                    "src": data[i].images.fixed_height_small_still.url,
+                    "move-src": data[i].images.fixed_height_small.url,
+                    "static-src": data[i].images.fixed_height_small_still.url,
+                    "data-state": "still",
+                });
+                gifHolder.html("");
+                gifHolder.append(image);
+                gifHolder.append(p);
+                $(".gif-content").prepend(gifHolder);
+
+            }
+        }
+
+    });    //end of 'then' section
+
+});
+
+
+
+// changes the moving/still state of my gif images
+$(".gif").val(this).on("click", function () {
+    // The attr jQuery method allows us to get or set the value of any attribute on our HTML element
+    var state = $(this).attr("data-state");
+    console.log(this);
+    console.log($(".gif"));
+    console.log($(".gif").val());
+    // If the clicked image's state is still, update its src attribute to what its data-animate value is.
+    // Then, set the image's data-state to animate
+    // Else set src to the data-still value
+    if (state === "still") {
+        $(this).attr("src", $(this).attr("move-src"));
+        $(this).attr("data-state", "move");
+    } else {
+        $(this).attr("src", $(this).attr("static-src"));
+        $(this).attr("data-state", "still");
+    }
+}); //end of statechange
+
+
+
+$("#the-search-button").on("click", function () {
+    event.preventDefault();
+    userInput = $("input").val();
+    inputCheck();
+    $("#accordionHolder").text("");
+    pushAccordion();
+    console.log(userInput);
+    console.log(topics);
 });
 
 
